0ec64039b4a5cbfb1e2245926218d060e96aa189
Carmine Zaccagnino <carmine@pc-carmine.localdomain>
2017-10-07 16:26:17 +0200

    Corretta indentazione e aggiunti ouput a console per debug al vecchio script

--- conversione.js
+++ conversione.js
@@ -1,4 +1,5 @@
 function vedisebin(numerodacontrollare) {
+  console.log("avviato vedisebin");
   while(numerodacontrollare>0)  {
     if(numerodacontrollare%10!=0 && numerodacontrollare%10!=1)  {
       return false;
@@ -9,21 +10,24 @@ function vedisebin(numerodacontrollare) {
 }
 
 function indec(numerobin) {                       // indec() e inbin() non sono difficili da spiegare in fondo
+  console.log("avviato indec");
   var numerocorrente = numerobin;
   var numerodauscire = 0;
   var i = 0;
   if (vedisebin(numerobin)) {
-  while(numerocorrente>0) {
-    numerodauscire=numerodauscire+numerocorrente%10*Math.pow(2,i);
-    i++;
-    numerocorrente=parseInt(numerocorrente/10);
-  }
-  return numerodauscire;
-} else {
+  console.log("uscito da vedisebin");
+    while(numerocorrente>0) {
+      numerodauscire=numerodauscire+numerocorrente%10*Math.pow(2,i);
+      i++;
+      numerocorrente=parseInt(numerocorrente/10);
+    }
+    return numerodauscire;
+  } else {
     return "non va bene";
   }
 }
 function inbin(numerodec) {                     // indec() e inbin() non sono difficili da spiegare in fondo
+  console.log("avviato inbin");
   var numerocorrente = numerodec;
   var resti = 0;
   var contatore = 1;
@@ -35,14 +39,16 @@ function inbin(numerodec) {                     // indec() e inbin() non sono di
   return resti;
 }
 function conversionamento(numerodaconvertire,bindec) {  // Il numero 1 o 0 passato prima questa funzione lo chiama bindec,
-  if(bindec==1) {                                       // se è uguale a 1 esegue la funzione indec(), se è 0 esegue la funzione
-    return indec(numerodaconvertire);                   // inbin()
+  console.log("avviato conversionamento");              // se è uguale a 1 esegue la funzione indec(), se è 0 esegue la funzione
+  if(bindec==1) {                                       // inbin()
+    return indec(numerodaconvertire);
   }
   if(bindec==0) {
     return inbin(numerodaconvertire);
   }
 }
 function processForm(e) {
+  console.log("avviato processForm");
       if (e.preventDefault) e.preventDefault(); // Per evitare che il form provi a fare cose che non deve fare
       var decbin = document.getElementById("decbin").checked; // la variabile decbin è una booleana che è true se il tasto decimale->binario è stato cliccato
       var bindec = document.getElementById("bindec").checked; // la variabile bindec è una booleana che è true se il tasto binario->decimale è stato cliccato
